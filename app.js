bf0100f5cbd3d5f49ee0e07134336d4741a7fa1e
thanhtai123-nguyen <ntai9621@gmail.com.vn>
2023-09-21 14:27:44 +0700

    cai dat xu lý lỗi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const contactsRouter = require ("./app/routes/contact.route");
 const cors =require("cors");
+const ApiError = require("./app/api-error");
 const app =express();
 app.use(cors());
 app.use(express.json());
@@ -8,4 +9,16 @@ app.use("/api/contacts", contactsRouter);
 app.get("/",(req, res)=>{
     res.json({ message:"Welcome to contact book application."});
 });
+
+app.use((req,res,next)=>{
+    return next (new ApiError(404,"Resource not found"));
+});
+app.use((err,req,res,next)=>{
+    return res.status(Error.statusCode || 500).json({
+        message: Error.message || "Internal Server Error",
+    });
+});
+
+
+
 module.exports =app;
