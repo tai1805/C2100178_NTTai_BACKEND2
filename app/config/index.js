07d932dd8386d737f2d9352a01f35bcfa73c658f
thanhtai123-nguyen <ntai9621@gmail.com.vn>
2023-10-02 19:27:19 +0700

    cai dat

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,10 @@
 const config ={
     app: {
         port: process.env.PORT || 3000,
+    },
+    db: {
+        uri: process.env.MONGGODB_URI || "mongodb://127.0.1:27017/contactbook"
     }
 };
+
 module.exports = config;
---
+++ app/config/utils/mongodb.util.js
@@ -0,0 +1,10 @@
+const{MongodbClient} = require("mongodb");
+
+class MongoDB {
+    static connect = async (uri)=>{
+        if(this.client) return this.client;
+        this.client = await MongodbClient.connect(uri);
+        return this.client;
+    };
+}
+module.exports=MongoDB;
