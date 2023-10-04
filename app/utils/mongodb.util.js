868fc25f104f43f32d95699fdf6aeaef06813dd8
thanhtai123-nguyen <ntai9621@gmail.com.vn>
2023-10-04 20:14:11 +0700

    cai dat handler

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,10 +1,10 @@
-const {MongodbClient} = require("mongodb");
-
+const{MongoClient} = require("mongodb");
 class MongoDB {
-    static connect = async (uri)=>{
-        if(this.client) return this.client;
-        this.client = await MongodbClient.connect(uri);
+
+    static connect = async (uri) => {
+        if (this.client) return this.client;
+        this.client = await MongoClient.connect(uri);
         return this.client;
     };
 }
-module.exports=MongoDB;
+module.exports = MongoDB;
